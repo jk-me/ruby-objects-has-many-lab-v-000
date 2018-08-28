a1f0888d43256d97c495a23bf7611d3731c774a3
Jenny K <jkam17@gmail.com>
2018-08-28 01:09:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,12 @@ class Song
     @name=name
     @@all << self 
   end
-  def name 
+  def artist_name
+    if @artist = nil 
+      return nil
+    else 
+      @artist.name 
+    end
+  end
     
 end 
